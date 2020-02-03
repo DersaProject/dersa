42b531ea02cc7d9181758e07a77bfc8224c8cb18
Dersa Admin <admin@dersa.ru>
2020-02-03 20:58:46 +0400

    no message

---
+++ Dersa.Common/Interfaces/IEditControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dersa.Interfaces
+{
+    public interface IEditControl
+    {
+        string dataField { get; set; }
+        #region Методы
+        string GenerateCS();
+        string CSTypeName();
+        #endregion
+    }
+}
