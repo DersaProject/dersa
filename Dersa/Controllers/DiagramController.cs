2f974ec023b776974ac5e2c5219130aa7fef87ab
Dersa Admin <admin@dersa.ru>
2020-02-15 16:27:39 +0400

    diagram get JSON

--- Dersa/Controllers/DiagramController.cs
+++ Dersa/Controllers/DiagramController.cs
@@ -2,9 +2,9 @@ using System.Reflection;
 using System.Data;
 using System.Linq;
 using System.Web.Mvc;
-using DIOS.Common.Interfaces;
 using Dersa.Models;
-
+using Dersa.Common;
+using Newtonsoft.Json;
 
 namespace Dersa.Controllers
 {
@@ -19,17 +19,39 @@ namespace Dersa.Controllers
 
 		}
 
-	/*  code template for controller Adapter
-		public string Create(int parent)
-		{
-			ResponseMaker M = new ResponseMaker(new ReceiveResponseHandler(doTrueResponseForCreate));
-			return M.MakeResponse();
-		}
-		private string doTrueResponseForCreate()
-		{
-		}
-	*/
-		public string Save(string id, string xml)
+        public string GetJson(string id)
+        {
+            string userName = HttpContext.User.Identity.Name;
+            DersaSqlManager DM = new DersaSqlManager();
+            DataTable T = DM.ExecuteMethod("DIAGRAM", "GetEntities", new object[] { id, userName, DersaUtil.GetPassword(userName) });
+            int appIndex = 0;
+            var query = from DataRow R in T.Rows
+                        select new
+                        {
+                            displayed_name = R["name"],
+                            id = R["id"],
+                            app_index = appIndex++,
+                            left = R["left"],
+                            top = R["top"],
+                            width = R["width"],
+                            height = R["height"],
+                            is_selected = false,
+                            is_visible = true
+                        };
+            return JsonConvert.SerializeObject(query);
+        }
+
+        /*  code template for controller Adapter
+            public string Create(int parent)
+            {
+                ResponseMaker M = new ResponseMaker(new ReceiveResponseHandler(doTrueResponseForCreate));
+                return M.MakeResponse();
+            }
+            private string doTrueResponseForCreate()
+            {
+            }
+        */
+        public string Save(string id, string xml)
 		{
 
 			string result = (new DiagramControllerAdapter()).Save(id, xml);
