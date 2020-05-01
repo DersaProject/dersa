da76b334be4c1f253b81d32cc39d98607346df5b
Dersa Admin <admin@dersa.ru>
2020-05-01 16:45:35 +0400

    Dersa_N (Nancy framefork)

---
+++ Dersa_N/Modules/HomeModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace NancyApp
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            //Get("/", param => "I'm Nancy Self Host Application.");
+            Get("/", parameters => {
+                return View["index.cshtml", null];
+            });
+        }
+    }
+}
---
+++ Dersa_N/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Nancy.ViewEngines.Razor;
+
+[assembly: OwinStartup(typeof(Dersa_N.Startup))]
+
+namespace Dersa_N
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+            //app.Run(context =>
+            //{
+            //    context.Response.ContentType = "text/html; charset=utf-8";
+            //    return context.Response.WriteAsync("<h2>Привет мир!</h2>");
+            //});
+        }
+    }
+
+    public class RazorConfig : IRazorConfiguration
+    {
+        public IEnumerable<string> GetAssemblyNames()
+        {
+            yield return "System.Data";
+            yield return "System.Web.Optimization";
+            yield return "Newtonsoft.Json";
+        }
+
+        public IEnumerable<string> GetDefaultNamespaces()
+        {
+            yield return "System.Data";
+            yield return "System.Web.Optimization";
+            yield return "Newtonsoft.Json";
+        }
+
+        public bool AutoIncludeModelNamespace
+        {
+            get { return true; }
+        }
+    }
+}
