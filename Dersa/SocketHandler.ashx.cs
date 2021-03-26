c3709a066de9c6ae39a44885ca56b05fbb12b8fc
Dersa Admin <admin@dersa.ru>
2021-03-26 19:24:52 +0300

    user name in WebSocket handler

--- Dersa/SocketHandler.ashx.cs
+++ Dersa/SocketHandler.ashx.cs
@@ -21,9 +21,11 @@ namespace Dersa
     public class SocketHandler : IHttpHandler
     {
         private int ticker = 0;
-        private System.Timers.Timer theTimer = new System.Timers.Timer(1000);
+        //private System.Timers.Timer theTimer = new System.Timers.Timer(1000);
         private AspNetWebSocketContext lastContext;
         private string entityId;
+        private ArraySegment<byte> readBuffer = new ArraySegment<byte>(new byte[1024]);
+        private Task<WebSocketReceiveResult> readTask;
 
         private ArraySegment<byte> GetResponse(ArraySegment<byte> src, int count)
         {
@@ -53,9 +55,10 @@ namespace Dersa
         private async Task WebSocketRequest(AspNetWebSocketContext context)
         {
             lastContext = context;
+            string userName = context.User.Identity.Name;
             //Получаем сокет клиента из контекста запроса
             var socket = context.WebSocket;
-            theTimer.Elapsed += TheTimer_Elapsed;
+            //theTimer.Elapsed += TheTimer_Elapsed;
             //theTimer.Start();
             while (true)
             {
@@ -78,13 +81,14 @@ namespace Dersa
                         DersaIO
                     {
                         ReadLine = () => ReceiveTextFromClient() ,
+                        ReadLineVirtually = () => ReceiveTextFromClientVirtually(),
                         WriteLine = s => SendTextToClient(s)
                     };
                     //{
                     //    ReadLine = new Func<string>(() => { return ReceiveTextFromClient(); }),
                     //    WriteLine = new Action<string>((s) => SendTextToClient(s))
                     //};
-                    object scriptResult = MIinner.Item2.Invoke(MIinner.Item1, new object[] { communicationObject });
+                    object scriptResult = MIinner.Item2.Invoke(MIinner.Item1, new object[] { userName, communicationObject });
                     string responseString = scriptResult is string ? (string)scriptResult : "result of execution " + entityId + " is not a string";
 
                     //object scriptResult = QueryControllerAdapter.GetMethodResult("Exec", int.Parse(entityId), "[0]");
@@ -108,22 +112,21 @@ namespace Dersa
                 }
             }
         }
-        private void TheTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            ticker++;
-            if (ticker > 5)
-                ticker = 1;
-            SendTextToClient(ticker.ToString());
-        }
+        //private void TheTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        //{
+        //    ticker++;
+        //    if (ticker > 5)
+        //        ticker = 1;
+        //    SendTextToClient(ticker.ToString());
+        //}
 
         private string ReceiveTextFromClient()
         {
             var socket = lastContext.WebSocket;
-            var buffer = new ArraySegment<byte>(new byte[1024]);
             // Ожидаем данные от него
-            var readResult = socket.ReceiveAsync(buffer, CancellationToken.None);
+            var readResult = socket.ReceiveAsync(readBuffer, CancellationToken.None);
             readResult.Wait();
-            string result = Encoding.UTF8.GetString(buffer.ToArray<byte>(), 0, readResult.Result.Count);
+            string result = Encoding.UTF8.GetString(readBuffer.ToArray<byte>(), 0, readResult.Result.Count);
             return result;
         }
         private async Task SendTextToClient(string text)
@@ -131,6 +134,25 @@ namespace Dersa
             var socket = lastContext.WebSocket;
             var sendBuff = new ArraySegment<byte>(Encoding.UTF8.GetBytes(text));
             socket.SendAsync(sendBuff, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        private string ReceiveTextFromClientVirtually()
+        {
+            //если пока ничего не пришло с клиента = возвращаем null
+            //если с клиента что-то пришло - обновляем таск и возвращаем то, чо пришло
+            var socket = lastContext.WebSocket;
+            if (readTask == null)
+            {
+                readTask = socket.ReceiveAsync(readBuffer, CancellationToken.None);
+            }
+            if (readTask.Status == TaskStatus.RanToCompletion || readTask.Status == TaskStatus.Faulted || readTask.Status == TaskStatus.Canceled)
+            {
+                string result = Encoding.UTF8.GetString(readBuffer.ToArray<byte>(), 0, readTask.Result.Count);
+                readTask = socket.ReceiveAsync(readBuffer, CancellationToken.None);//ставим новую задачу на чтение
+                return result;
+            }
+            else
+                return null;
         }
     }
 }
